89d478e33994beeb10408c8a7c43b297eda6ab7b
Luis Angel Sandoval Medina <sandovalguichoo@gmail.com>
2023-05-02 10:55:44 -0600

    router-link-ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { VistaHomeComponent } from './vistas/vista-home/vista-home.component';
 import { HeaderComponent } from './vistas/header/header.component';
 import { FooterComponent } from './vistas/footer/footer.component';
+import { LoginComponent } from './vistas/login/login.component';
+import { MiPerfilComponent } from './vistas/mi-perfil/mi-perfil.component';
 
-
+const routes: Routes=[
+  {path:'', component:VistaHomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'mi-perfil', component:MiPerfilComponent}
+]
 
 
 @NgModule({
@@ -17,12 +23,20 @@ import { FooterComponent } from './vistas/footer/footer.component';
     AppComponent,
     VistaHomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    MiPerfilComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule
+    BrowserModule,
+    FormsModule,
+     HttpClientModule, 
+     ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
